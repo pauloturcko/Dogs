27a6a3e8423d1660b20cdce3edcb03a1fc5cfe8a
Paulo Turco <pauloturcko@gmail.com>
2025-01-07 21:11:05 -0300

    Header finished & error fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import styles from '../Styles/Header.module.css'
-import { Link } from 'react-router-dom'
-import { ReactComponent as Dogs } from '../Assets/dogs.svg'
+import React from 'react';
+import styles from '../Styles/Header.module.css';
+import { Link } from 'react-router-dom';
+import Dogs from '../Assets/dogs.svg';
+
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav className='container'>
-        <Link to="/">
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
           <Dogs />
         </Link>
-        <Link to="/login">Login / Criar</Link>
+        <Link className={styles.login} to="/login">
+          Login / Criar
+        </Link>
       </nav>
-    </div>
-  )
-}
+    </header>
+  );
+};
 
-export default Header
+export default Header;
