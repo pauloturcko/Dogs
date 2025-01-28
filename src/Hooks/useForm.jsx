312d086d403f1bdc134fb40c8347960453dd2e4f
Paulo Turco <pauloturcko@gmail.com>
2025-01-28 20:57:01 -0300

    Photo Post

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -5,16 +5,20 @@ const types = {
     regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     ,
     message: 'Preencha um email válido'
+  },
+  number: {
+    regex: /^\d+$/,
+    message: 'Utilize apenas números'
   }
 }
 
 const useForm = (type) => {
-  const [ value, setValue ] = useState('')
-  const [ error, setError ] = useState(null)
+  const [value, setValue] = useState('')
+  const [error, setError] = useState(null)
 
   function validate(value) {
-    if(type === false) return false;
-    if(value.length === 0) {
+    if (type === false) return false;
+    if (value.length === 0) {
       setError('Preencha um valor')
       return false
     } else if (types[type] && !types[type].regex.test(value)) {
@@ -26,8 +30,8 @@ const useForm = (type) => {
     }
   }
 
-  function onChange ({target}) {
-    if(error) validate(target.value)
+  function onChange({ target }) {
+    if (error) validate(target.value)
     setValue(target.value)
   }
 
